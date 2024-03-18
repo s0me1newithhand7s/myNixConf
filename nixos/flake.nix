3faa60c52e0ba862ec70ec7e5c5f05c94cadf307
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-03-18 19:29:51 +0300

    Update flake.nix
    fixed error

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,7 +3,7 @@
 {
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-        nixpkgs-master.url. = "github:nixos/nixpkgs/master";
+        nixpkgs-master.url = "github:nixos/nixpkgs/master";
 
         home-manager = {
             url = "github:nix-community/home-manager/";
