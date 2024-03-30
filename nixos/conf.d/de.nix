bc26d422cbed54c3ca6fffef175e91494f066ac7
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-03-30 21:39:25 +0300

    re-added recreated de.nix and wm.nix
    after testing session i've came up with idea of creating this monsters. tl;dr - kinda wrong way to use nixos when you have homeManager, but i prefer to show one way that will lead to further searching.

---
+++ nixos/conf.d/de.nix
@@ -0,0 +1,34 @@
+{ config, ... }:
+
+{
+    services.xserver = {
+        enable = true;
+            displayManager = {
+            autoLogin.enable = false;
+            gdm.enable = false;
+            lightdm.enable = false;
+            sddm = {
+                enable = false;
+                wayland.enable = false;
+            };
+            startx.enable = false;
+            xpra.enable = false;
+        };
+        desktopManager = {
+            budgie.enable = false;
+            cinnamon.enable = false;
+            deepin.enable = false;
+            enlightment.enable = false;
+            gnome.enable = false;
+            kodi.enable = false;
+            lumina.enable = false;
+            lxqt.enable = false;
+            plasma5.enable = false;
+            plasma6.enable = false;
+            mate.enable = false;
+            posh.enable = false;
+            retroarch.enable = false;
+            xterm.enable = false;
+        };
+    };
+}
