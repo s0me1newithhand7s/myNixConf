d0de45354764454156f7aedc7b24a8feb481278a
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-03-30 21:49:31 +0300

    reuploaded desktop.d
    xd i forgor

---
+++ nixos/desktop.d/wm.nix
@@ -0,0 +1,61 @@
+{ config, ... }:
+
+{
+    programs = {
+        cage.enable = false;
+        hyprland.enable = false;
+        labwc.enable = false;
+        river.enable = false;
+        sway.enable = false;
+        wayfire.enable = false;
+    };
+    services.xserver = {
+        enable = true;
+        windowManager = {
+            afterstep.enable = false;
+            awesome.enable = false;
+            berry.enable = false;
+            bspwm.enable = false;
+            clfswm.enable = false;
+            cwm.enable = false;
+            dwm.enable = false;
+            e16.enable = false;
+            evilwm.enable = false;
+            exwm.enable = false;
+            fluxbox.enable = false;
+            fvwm2.enable = false;
+            fvwm3.enable = false;
+            hackednox.enable = false;
+            herbstluftwm.enable = false;
+            hypr.enable = false;
+            i3.enable = false;
+            icewm.enable = false;
+            jwm.enable = false;
+            katariawm.enable = false;
+            leftwm.enable = false;
+            lwm.enable = false;
+            metacity.enable = false;
+            mlvwm.enable = false;
+            mwm.enable = false;
+            nimdow.enable = false;
+            notion.enable = false;
+            openbox.enable = false;
+            pekwm.enable = false;
+            qtile.enable = false;
+            ragnarwm.enable = false;
+            ratpoison.enable = false;
+            sawfish.enable = false;
+            smallwm.enable = false;
+            spectrwm.enable = false;
+            stumpwm.enable = false;
+            tinywm.enable = false;
+            twm.enable = false;
+            windowlab.enable = false;
+            windowmaker.enable = false;
+            wmderland.enable = false;
+            wmii.enable = false;
+            xmonad.enable = false;
+            yeahwm.enable = false;
+        };
+    };
+}
