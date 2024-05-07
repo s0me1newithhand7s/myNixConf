d3381fbdfe05f4a1db236fce944a8d00c2308bb8
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-05-07 11:52:06 +0300

    Update de.nix
    hot (xd) fix of major one error in syntaxis

--- nixos/desktop.d/de.nix
+++ nixos/desktop.d/de.nix
@@ -1,23 +1,26 @@
 # de and dm options
 
 {
-    services =
+    services = {
         desktopManager = {
             plasma6.enable = false;
+        };
         xserver = {
             enable = true;
-            budgie.enable = false;
-            cinnamon.enable = false;
-            deepin.enable = false;
-            enlightenment.enable = false;
-            gnome.enable = false;
-            kodi.enable = false;
-            lumina.enable = false;
-            lxqt.enable = false;
-            plasma5.enable = false;
-            mate.enable = false;
-            retroarch.enable = false;
-            xterm.enable = false;
+            desktopManager = {
+                budgie.enable = false;
+                cinnamon.enable = false;
+                deepin.enable = false;
+                enlightenment.enable = false;
+                gnome.enable = false;
+                kodi.enable = false;
+                lumina.enable = false;
+                lxqt.enable = false;
+                plasma5.enable = false;
+                mate.enable = false;
+                retroarch.enable = false;
+                xterm.enable = false;
+            };
         };
     };
 }
