f82d2e5abbbc20360a91d8044c1a48f482a53142
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-05-17 14:34:27 +0300

    Delete home.modules directory

--- home.modules/modules.d/foot.nix
+++
@@ -1,6 +0,0 @@
-{
-    programs.foot = {                    # about foot - https://codeberg.org/dnkl/foot
-        enable = true;                   # enabling toggle
-        settings = { };                  # settings for foot, or config itself 
-    };
-}
--- home.modules/modules.nix
+++
@@ -1,8 +0,0 @@
-{
-    imports = [
-        ./modules.d/helix.nix
-        ./modules.d/eza.nix
-        ./modules.d/foot.nix
-        ./modules.d/starship.nix
-    ];
-}
