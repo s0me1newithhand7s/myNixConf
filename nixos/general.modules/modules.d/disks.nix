7b1a539f086f92b9721cfc4fd3a1703d5ddcc7f7
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-06-01 22:30:14 +0300

    edtied disks.nix

--- nixos/general.modules/modules.d/disks.nix
+++ nixos/general.modules/modules.d/disks.nix
@@ -1,15 +1,17 @@
 { config, lib, pkgs, ... }:
 
 {
-    fileSystems."/" = { 
-        device = "/dev/disk/by-label/nixos";
-        # fsType = " ";       # <= here i reccomend to choose either ext4 or f2fs
+    fileSystems = {
+        "/" = { 
+            device = "/dev/disk/by-label/nixos";
+            # fsType = " ";       # <= here i reccomend to choose either ext4 or f2fs
+        };
+
+        "/boot" = {
+            device = "/dev/disk/by-label/boot";
+            fsType = "vfat";
+        };
     };
 
-    fileSystems."/boot" = {
-        device = "/dev/disk/by-label/boot";
-        fsType = "vfat";
-    };
-
-  swapDevices = [ ];
+    swapDevices = [ ];
 }
