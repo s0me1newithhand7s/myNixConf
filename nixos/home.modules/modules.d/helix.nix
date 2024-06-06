b57800e853971953a1f3df252a7928d4e8ad61f5
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-06-06 20:43:52 +0300

    Delete nixos/home.modules/modules.d/helix.nix

--- nixos/home.modules/modules.d/helix.nix
+++
@@ -1,7 +0,0 @@
-{
-    programs.helix = {                     # about helix - https://helix-editor.com/
-        defaultEditor = true;              # toggle for making it default editor
-        enable = true;                     # enabling toggle
-        settings = { };                    # settings for helix, or config itself 
-    };
-}
