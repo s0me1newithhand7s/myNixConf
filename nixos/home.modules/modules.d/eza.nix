bb0ae709e6b5be8c0ea7d875b08d52f43a7c08df
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-06-06 20:43:11 +0300

    Delete nixos/home.modules/modules.d/eza.nix

--- nixos/home.modules/modules.d/eza.nix
+++
@@ -1,9 +0,0 @@
-{
-    programs.eza = {                    # about eza - https://github.com/eza-community/eza
-        enable = true;                  # enabling toggle
-        enableBashIntegration = true;   # integration with bash
-        enableFishIntegration = true;   # integration with fish
-        enableZshIntegration = true;    # integration with zsh
-        icons = true;
-    };
-}
