cd0eaa917d647171b5cdd2715753db3c9228e729
aski425 <54905103+aski425@users.noreply.github.com>
2024-06-08 01:32:34 +0500

    Update dm.nix

--- nixos/host.modules/modules.d/dm.nix
+++ nixos/host.modules/modules.d/dm.nix
@@ -7,7 +7,7 @@
         xserver = {
             displayManager = {
                 gdm.enable = false;         # toggle for GDM
-                lightdm.enable = false;     # toggle for LightDM, default DM
+                lightdm.enable = true;     # toggle for LightDM, default DM
                 startx.enable = false;      # toggle for startx in case you'll need it
                 xpra.enable = false;        # toggle for xpra, lmao
                 sx.enable = false;          # sx is pseudo DM, but it is toggle for it
