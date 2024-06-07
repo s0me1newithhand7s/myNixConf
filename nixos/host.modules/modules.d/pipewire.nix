5a90adb5881bde6a86df228dd51f339ac806d52a
aski425 <54905103+aski425@users.noreply.github.com>
2024-06-08 01:30:46 +0500

    Create pipewire.nix

---
+++ nixos/host.modules/modules.d/pipewire.nix
@@ -0,0 +1,12 @@
+{ config, lib, ... }:
+
+{
+   services.pipewire = {
+      enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      pulse.enable = true;
+  };
+}
