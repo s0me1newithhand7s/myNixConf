e991a9c21fd40160b637c4a970f875c951908b05
aski425 <54905103+aski425@users.noreply.github.com>
2024-06-08 01:41:13 +0500

    Create yakuake.nix

---
+++ nixos/home.modules/yakuake.nix
@@ -0,0 +1,9 @@
+{
+    programs.yakuake = {
+        enable = true;                  # enabling toggle
+        enableBashIntegration = true;   # integration with bash
+        enableFishIntegration = true;   # integration with fish
+        enableZshIntegration = true;    # integration with zsh
+        icons = true;
+    };
+}
