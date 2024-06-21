c85de78b8395a43a91671483010dbf957eceeb73
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-06-21 20:57:21 +0300

    Update hardware.nix
    updated to match current unstable

--- nixos/host.modules/modules.d/hardware.nix
+++ nixos/host.modules/modules.d/hardware.nix
@@ -3,9 +3,9 @@
 {
     hardware = {
         # cpu = { };                # whacky and kinda hw-exclusive option. feel free to just copy-paste it from ur hardware-configuration.nix
-        opengl = {                  # cruical part for proper GPU working
+        graphics = {                  # cruical part for proper GPU working
 	        enable = true;
-            driSupport = true;
+            	enable32Bit = true;
         };
      };
 }
