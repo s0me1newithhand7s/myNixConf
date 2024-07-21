2e4084393b6a0113270255e777e758000204771d
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-07-21 15:03:34 +0300

    Update root.nix

--- host/os/users/root.nix
+++ host/os/users/root.nix
@@ -3,7 +3,7 @@
 }: {
     users = { 
         users = {
-            hand7s = {
+            root = {
                 description = "root";   # description for use xd
                 initialHashedPassword = "$y$j9T$5L3DsM6L47uZslAt2VF7X/$KebOq4k0jXKmV52tzViPaCm7j0hVPKV9hiJDNuthFE9";
                 # ^^^ "pwd" but hashed
