f10d00838e5fcfc6d2f04b301291bd5e7872c473
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-07-21 15:52:38 +0300

    Update amd.nix

--- host/hardware/drivers/amd.nix
+++ host/hardware/drivers/amd.nix
@@ -15,7 +15,6 @@
                     package = with pkgs; [ driversi686Linux.amdvlk ];
                 };
                 supportExperimental.enable = false;
-                initrd = true;
                 legacySupport.enable = false;
                 opencl.enable = false;
             };
