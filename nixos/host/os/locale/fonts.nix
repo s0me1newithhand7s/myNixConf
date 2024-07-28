d695bf7ca7d77afc0d0c0f961140d2a89269e791
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-07-28 18:23:08 +0300

    Update fonts.nix

--- nixos/host/os/locale/fonts.nix
+++ nixos/host/os/locale/fonts.nix
@@ -11,7 +11,7 @@
             enable = true;                  # more options - https://search.nixos.org/options?channel=unstable&from=0&size=50&sort=alpha_asc&type=packages&query=fonts.fontconfig
         };
         packages =  with pkgs; [
-            hack-font
+            
         ];
     };
 }
