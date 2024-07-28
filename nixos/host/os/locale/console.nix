dfbed37097af6cfecac611676ee2eda56b3184f4
hand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2024-07-28 18:23:25 +0300

    Update console.nix

--- nixos/host/os/locale/console.nix
+++ nixos/host/os/locale/console.nix
@@ -5,7 +5,7 @@
     console = {                                 # vconsole locales
         # colors = [  ];                        # vconsole colors, but i have starship for it
         # enable = true;                        # toggle for enabling vconsole
-        # font = "Hack-Regular";                # YES i love hack font
+        # font = "";
         # keyMap = "us";                        # keymap for vconsole
         useXkbConfig = true;                    # toggle for using xkb config
     }; 
